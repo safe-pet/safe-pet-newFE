e2b16c1895bf143791a6d996232c27395bab5449
LEE-YO-HAN <yhl0078@gmail.com>
2022-12-29 11:35:54 +0900

    husky test

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,8 +6,6 @@ import styles from "../styles/Home.module.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  console.log("test");
-  console.log(d1b21bd12b21);
   return (
     <>
       <Head>
