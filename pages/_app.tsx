1645ba812bc093cb124bcaf069fabd28248cd070
LEE-YO-HAN <yhl0078@gmail.com>
2022-12-30 12:25:50 +0900

    Update : add kakao sdk key Head set up in utils dir(/NextHead.tsx)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,15 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import Script from "next/script";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Script
+        src={`//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_KEY} &autoload=false`}
+        // strategy="beforeInteractive"
+      />
+      <Component {...pageProps} />;
+    </>
+  );
 }
