0fc24766bdfe7d4f80d21a47025e664624d72474
LEE-YO-HAN <yhl0078@gmail.com>
2023-01-01 22:43:07 +0900

    Update: image src, add mata:og tag

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,7 +9,7 @@ export default function App({ Component, pageProps }: AppProps) {
         src={`//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_KEY} &autoload=false`}
         // strategy="beforeInteractive"
       />
-      <Component {...pageProps} />;
+      <Component {...pageProps} />
     </>
   );
 }
--- src/utils/NextHead.tsx
+++ src/utils/NextHead.tsx
@@ -4,6 +4,9 @@ export const NextHead = (descrpition: string | any): JSX.Element => {
   return (
     <Head>
       <title>safe-pet</title>
+      <meta property="og:title" content="safe-pet" />
+      <meta property="og:description" content={descrpition} />
+      <meta property="og:image" content={require("../image/mainLogo.png")} />
       <meta name="description" content={descrpition} />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <link rel="icon" href="/favicon.ico" />
