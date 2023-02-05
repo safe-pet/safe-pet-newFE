237d838526c527e486553093fc284ef8e62af58b
LEE-YO-HAN <yhl0078@gmail.com>
2023-02-05 17:45:11 +0900

    Add: event slide Fix: some component name

--- src/components/mainPage/parcelOutPet/ParcelOutPet.tsx
+++ src/components/mainPage/parcelOutPet/ParcelOutPet.tsx
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 import Image from "next/image";
 
-export const SalePet = () => {
+export const ParcelOutPet = () => {
   return (
     <Container>
       <ContentWrap>
