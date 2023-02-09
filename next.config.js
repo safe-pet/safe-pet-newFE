4654f65d67cd696ee231a143d5cff58ee761c197
jeongwon <rladnjs15@naver.com>
2023-02-09 13:43:18 +0900

    feat: 카카오 맵 초기 설정

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: false,
   compiler: {
-    styledComponents: true,
+    styledComponents: true, // className do not match error 방지
   },
   env: {
     NEXT_PUBLIC_KAKAO_KEY: process.env.NEXT_PUBLIC_KAKAO_KEY,
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,17 +2,20 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Script from "next/script";
 import { Layout } from "../src/components/layout";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      {/* <Head> */}
+      {/* eslint-disable-next-line */}
       <Script
         // src={`//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_KEY} &autoload=false`}
         // src={`//dapi.kakao.com/v2/maps/sdk.js?appkey=0fdb896f2fde78f39d7e89967733a73d&libraries=services&autoload=false,clusterer`}
-        src={`//dapi.kakao.com/v2/maps/sdk.js?appkey=0fdb896f2fde78f39d7e89967733a73d&libraries=services&autoload=false`}
-
+        src={`http://dapi.kakao.com/v2/maps/sdk.js?appkey=0fdb896f2fde78f39d7e89967733a73d&libraries=services&autoload=false`}
         // strategy="beforeInteractive"
       />
+      {/* </Head> */}
 
       <Layout>
         <Component {...pageProps} />
